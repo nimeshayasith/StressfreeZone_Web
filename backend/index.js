e8dfedf6a9258c60fff403069ce71e5c8e30e0e4
kaveesha <kaveeshawijegurusinghe@gmail.com>
2024-07-31 23:30:11 +0530

    server creation

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,12 @@
-console.log("kaveesha");
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+
+app.use(cors())
+
+const port = 8080
+
+app.listen(port,()=>{
+    console.log(`server is running ${port}`)
+})
