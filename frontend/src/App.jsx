1e14ab1131fe11ca343f8c2425b71d615a20612f
Dayani-Hewawasam <hewawasam_adk_e23@engug.ruh.ac.lk>
2024-08-18 18:39:11 +0530

    home page -1 & 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './Components/Register';
 import Login from './Components/Login';
 import AdminLogin from './Components/Admin login';
+import Home from './Components/Home';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={< Login/>} />
+        <Route path="/" element={< Home/>} />
+        <Route path="/login" element={< Login/>} />
         <Route path="/register" element={<Register />} />
         <Route path="/adminlogin" element={<AdminLogin />} />
       </Routes>
